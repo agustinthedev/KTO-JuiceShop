ca58dce5440b0523f87c19bf053aedad11cde32d
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-19 18:43:40 -0300

    Basic functionality for startBrowser function

--- Test Suite/UI/Util/Framework.py
+++ Test Suite/UI/Util/Framework.py
@@ -1,6 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import random
+import string
+
 class Framework:
+
+    browser = ""
+
     def __init__(self):
         pass
+
+    def startBrowser(self, url):
+        self.browser = webdriver.Chrome()
+        self.browser.get(url)
+        return self.browser
 
     def generateEmail(self):
         pass
