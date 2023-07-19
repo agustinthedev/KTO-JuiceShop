aefebf56aa2e2ac4a87c72b93ccb44b0b0a7233a
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-19 18:40:49 -0300

    Basic structure for Framework file

---
+++ Test Suite/UI/Util/Framework.py
@@ -0,0 +1,12 @@
+class Framework:
+    def __init__(self):
+        pass
+
+    def generateEmail(self):
+        pass
+
+    def findElement(self, xpath):
+        pass
+
+    def clickElement(self, xpath):
+        pass
