08727a085deea67204dcfb3bb375aa612bb387c3
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-19 13:48:58 -0300

    Added comments in code for better understanding

--- Test Suite/UI/Register_001.py
+++ Test Suite/UI/Register_001.py
@@ -8,11 +8,13 @@ import string
 class Test:
     browser = ""
 
+    # Function to start the browser in the desired URL. Registration page in this case.
     def openBrowser(self, url):
         self.browser = webdriver.Chrome()
         self.browser.get(url)
         sleep(3)
 
+    # Generate a valid 12 characters gmail email each time.
     def generateEmail(self):
         email = ''
         for x in range(12):
@@ -20,13 +22,17 @@ class Test:
 
         return email + "@gmail.com"
 
+    # Main structure for test
     def startTest(self):
+        # Open browser and save instance on class variable
         url = Util.REGISTER_SITE_URL
         self.openBrowser(url)
 
+        # Data that will be used to create the user
         user_email = self.generateEmail()
         user_password = "1234567890"
         
+        # Try to click the 'Dismiss' button. If not able to, check if registration form element is present, if YES, continue, if NOT, stop test.
         try:
             dismiss_button = self.browser.find_element(By.XPATH, Util.DISMISS_BUTTON_XPATH)
             dismiss_button.click()
@@ -43,30 +49,38 @@ class Test:
                 print("Unable to find Registration Form DIV element, stopping the test.")
                 return False  
         
+        # Identify email input and write user email
         email_input = self.browser.find_element(By.XPATH, Util.EMAIL_FIELD_XPATH)
         email_input.send_keys(user_email)
-
+        
+        # Identify password input and write user password
         passwd_input = self.browser.find_element(By.XPATH, Util.PASSWD_FIELD_XPATH)
         passwd_input.send_keys(user_password)
 
+        # Identify repeat password input and write user password again
         repeat_passwd_input = self.browser.find_element(By.XPATH, Util.REPEAT_PASSWD_FIELD_XPATH)
         repeat_passwd_input.send_keys(user_password)
 
+        # Identify security questions element and click it so all options inside dropdown become clickable
         security_question_element = self.browser.find_element(By.XPATH, Util.SECURITY_QUESTION_ELEMENT_XPATH)
         security_question_element.click()
         sleep(2)
 
+        # Identify and click security answer option from dropdown
         security_question_answers_array = self.browser.find_elements(By.XPATH, Util.SECURITY_QUESTION_ANSWERS_XPATH)
         security_question_answers_array[0].click()
 
+        # Identify security question answer inpout and provide a valid response
         security_question_answer_input = self.browser.find_element(By.XPATH, Util.SECURITY_QUESTION_ANSWER_XPATH)
         security_question_answer_input.send_keys("Test valid response")
-
         sleep(2)
+
+        # Identify and click the Submit button
         submit_button = self.browser.find_element(By.XPATH, Util.SUBMIT_BUTTON_XPATH)
         submit_button.click()
         sleep(2)
 
+        # Get current URL after clicking the Submit button. If URL is login page, registration was successful
         current_url = self.browser.current_url
 
         if current_url == "https://juice-shop.herokuapp.com/#/login":
