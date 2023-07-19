70eb7df01d2555ae163bb361640106aa9812386c
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-19 14:06:08 -0300

    Basic functionality for creating user using API

--- Test Suite/API/Register_001.py
+++ Test Suite/API/Register_001.py
@@ -12,14 +12,37 @@ class Test:
         return email + "@gmail.com"
     
     def startTest(self):
+        user_email = self.generateEmail()
+        user_password = "1234567890"
+
         url = Util.REGISTER_API_URL
         data = Util.REGISTER_API_PAYLOAD
-        data["email"] = self.generateEmail()
-        data["password"] = "1234567890"
-        data["passwordRepeat"] = "1234567890"
+        data["email"] = user_email
+        data["password"] = user_password
+        data["passwordRepeat"] = user_password
 
         request = requests.post(url, json=data)
-        print(request.status_code)
+        status_code = request.status_code
+
+        if status_code == 201:
+            print("=================================================")
+            print("New user created successfully")
+            print("Credentials used:")
+            print(f"    User email: {user_email}")
+            print(f"    User password: {user_password}")
+            print("Stopping test.")
+            print("=================================================")
+
+            return True
+        else:
+            print("=================================================")
+            print(f"Unable to create a new user using the URL: {url}")
+            print(f"Status code: {status_code}")
+            print(f"Data dictionary used: {data}")
+            print("Stopping test.")
+            print("=================================================")
+
+            return False
     
 test = Test()
 test.startTest()
