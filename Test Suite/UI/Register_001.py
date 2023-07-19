2a86f703a3b718e70a88c1a0d717fc8496d86cf6
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-19 12:58:58 -0300

    First validationds, dismiss button and form element present

--- Test Suite/UI/Register_001.py
+++ Test Suite/UI/Register_001.py
@@ -9,10 +9,28 @@ class Test:
     def openBrowser(self, url):
         self.browser = webdriver.Chrome()
         self.browser.get(url)
+        sleep(3)
 
     def startTest(self):
         url = Util.REGISTER_SITE_URL
         self.openBrowser(url)
+        
+        try:
+            dismiss_button = self.browser.find_element(By.XPATH, Util.DISMISS_BUTTON_XPATH)
+            dismiss_button.click()
+
+            print("Clicking Dismiss button.")
+        except Exception as e:
+            print("Unable to click Dismiss button. Identifying if other elements are present in order to continue or stop the test.")
+            
+            try:
+                self.browser.find_element(By.XPATH, Util.REGISTRATION_FORM_DIV_XPATH)
+
+                print("Registration Form DIV element present, continuing.")
+            except Exception as e:
+                print("Unable to find Registration Form DIV element, stopping the test.")
+                return False
+
         sleep(10)
 
 
--- Test Suite/UI/Util/Util.py
+++ Test Suite/UI/Util/Util.py
@@ -1,4 +1,5 @@
-REGISTER_SITE_URL = "https://juice-shop.herokuapp.com/#/register"
+REGISTER_SITE_URL = "https://google.com"
 LOGIN_SITE_URL = "https://juice-shop.herokuapp.com/#/login"
 
 DISMISS_BUTTON_XPATH = "//span[text()='Dismiss']"
+REGISTRATION_FORM_DIV_XPATH = "//div[@id='registration-form']"
