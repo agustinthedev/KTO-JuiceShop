bd484a0f2b67ed0ff9c2adb169f4d4a161a99084
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-19 12:45:36 -0300

    Validation

--- Test Suite/UI/Register_001.py
+++ Test Suite/UI/Register_001.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from Util import Util
+from time import sleep
 
 class Test:
     browser = ""
@@ -9,4 +11,10 @@ class Test:
         self.browser.get(url)
 
     def startTest(self):
-        pass
+        url = Util.REGISTER_SITE_URL
+        self.openBrowser(url)
+        sleep(10)
+
+
+test = Test()
+test.startTest()
