5ac48db2993b77d3185fdfdd61eaae4b38907fb2
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-19 14:01:03 -0300

    Basic structure for register happy path API test

--- Test Suite/API/Register_001.py
+++ Test Suite/API/Register_001.py
@@ -1,1 +1,25 @@
+from Util import Util
 import requests
+import random
+import string
+
+class Test:
+    def generateEmail(self):
+        email = ''
+        for x in range(12):
+            email+=''.join(random.choice(string.ascii_lowercase))
+
+        return email + "@gmail.com"
+    
+    def startTest(self):
+        url = Util.REGISTER_API_URL
+        data = Util.REGISTER_API_PAYLOAD
+        data["email"] = self.generateEmail()
+        data["password"] = "1234567890"
+        data["passwordRepeat"] = "1234567890"
+
+        request = requests.post(url, json=data)
+        print(request.status_code)
+    
+test = Test()
+test.startTest()
---
+++ Test Suite/API/Util/Util.py
@@ -0,0 +1,13 @@
+REGISTER_API_URL = "https://juice-shop.herokuapp.com/api/Users/"
+REGISTER_API_PAYLOAD = {
+  "email": "",
+  "password": "",
+  "passwordRepeat": "",
+  "securityQuestion": {
+    "id": 1,
+    "question": "Your eldest siblings middle name?",
+    "createdAt": "2023-07-19T16:46:36.489Z",
+    "updatedAt": "2023-07-19T16:46:36.489Z"
+  },
+  "securityAnswer": "123123"
+}
