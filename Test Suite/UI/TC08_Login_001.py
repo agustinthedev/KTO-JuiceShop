784a208efe953ebeee89bdcbf592cd10b0fb7cea
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-27 02:52:24 -0300

    Improved structure for TC08

--- Test Suite/UI/TC08_Login_001.py
+++ Test Suite/UI/TC08_Login_001.py
@@ -3,18 +3,25 @@ from selenium.webdriver.common.by import By
 from time import sleep
 from Util import Util
 from Util import Framework
+import requests
 
 class Test:
-    test_name = ""
     browser = ""
 
-    def __startBrowser__(self, url):
-        self.browser = webdriver.Chrome()
-        self.browser.get(url)
-        sleep(3)
-
     def startTest(self):
+        test_name = "Login_001 (UI)"
+
         # Initialize framework and log message
         fr = Framework.Framework()
         fr.log("====================================================================")
         fr.log(f"Starting execution for test: {self.test_name}")
+
+        # Open browser and navigate to Register url
+        url = Util.REGISTER_SITE_URL
+        self.browser = fr.startBrowser(url)
+
+        # Data that will be used to create the user
+        user_email = fr.generateEmail()
+        user_password = "123123"
+
+        # Create a brand new account that will be used to log in
