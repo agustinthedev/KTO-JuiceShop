a66158f90e86cd1f0dc1f7c2a0ab84feae173a08
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-27 01:16:03 -0300

    Added test file ofr TC13

---
+++ Test Suite/API/TC13_Register_003.py
@@ -0,0 +1,44 @@
+from Util import Util, Framework
+import requests
+import random
+import string
+
+class Test:
+    def startTest(self):
+        test_name = "Register_003 (API)"
+        user_email = Framework.generateEmail()
+        user_password = "123"
+
+        url = Util.REGISTER_API_URL
+        data = Util.REGISTER_API_PAYLOAD
+        data["email"] = user_email
+        data["password"] = user_password
+        data["passwordRepeat"] = user_password
+
+        request = requests.post(url, json=data)
+        status_code = request.status_code
+
+        if status_code == 201:
+            print("=================================================")
+            print("It was possible to create a new user using an invalid password, test failed.")
+            print("Credentials used:")
+            print(f"    User email: {user_email}")
+            print(f"    User password: {user_password}")
+            print("Stopping test.")
+            print("=================================================")
+
+            return Framework.getReturnData(False, f"[✖] ({test_name}) User created successfully using invalid password, test failed.")
+        else:
+            print("=================================================")
+            print(f"Unable to create a new user using an invalid password, test passed.")
+            print(f"Status code: {status_code} || Request URl: {url}")
+            print("Credentials used:")
+            print(f"    User email: {user_email}")
+            print(f"    User password: {user_password}")
+            print("Stopping test.")
+            print("=================================================")
+
+            return Framework.getReturnData(True, f"[✔] ({test_name}) Unable to create a new user using invalid password, test passed.")
+        
+test = Test()
+test.startTest()
