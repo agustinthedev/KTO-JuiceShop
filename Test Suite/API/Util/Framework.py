028bbe6a96e2bde88ec708b30d2dee66113ecdd9
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-27 01:18:30 -0300

    Added log function to framework file

--- Test Suite/API/Util/Framework.py
+++ Test Suite/API/Util/Framework.py
@@ -1,10 +1,13 @@
 import random
 import string
+import datetime
 
 def getReturnData(passed, message):
     data = {}
     data["passed"] = passed
     data["message"] = message
+    
+    log(message)
 
     return data
 
@@ -14,3 +17,13 @@ def generateEmail():
             email+=''.join(random.choice(string.ascii_lowercase))
 
         return email + "@gmail.com"
+
+# Logs message to console and also appends it to logs.txt file
+def log(self, text):
+    now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+    message = f"{now} {text}"
+    print(message)
+
+    with open("logs.txt", "a") as file:
+        file.write(message + "\n")
