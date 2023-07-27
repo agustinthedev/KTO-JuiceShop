07d78492ca33cd11a3900299c222e8f1548125f1
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-27 03:11:18 -0300

    Fixed test name

--- Test Suite/UI/TC09_Login_002.py
+++ Test Suite/UI/TC09_Login_002.py
@@ -9,7 +9,7 @@ class Test:
     browser = ""
 
     def startTest(self):
-        test_name = "Login_001 (UI)"
+        test_name = "Login_002 (UI)"
 
         # Initialize framework and log message
         fr = Framework.Framework()
