d4acdf29b8f9520f5c4bebf6822af2fc3263ede3
Agustin Guarino <agustinguarino01@gmail.com>
2023-07-27 03:42:23 -0300

    Added new test to API test suite executor

--- Test Suite/Run API Test Suite.py
+++ Test Suite/Run API Test Suite.py
@@ -1,11 +1,11 @@
-from API import TC11_Register_001, TC12_Register_002, TC13_Register_003, TC14_Register_004, TC15_Register_005, TC16_Register_006, TC17_Register_007, TC18_Login_001, TC19_Login_002, TC20_Login_003
+from API import TC11_Register_001, TC12_Register_002, TC13_Register_003, TC14_Register_004, TC15_Register_005, TC16_Register_006, TC17_Register_007, TC18_Login_001, TC19_Login_002, TC20_Login_003, TC21_Register_008
 
 
 class APISuite():
     test_cases = []
 
     def __init__(self):
-        self.test_cases.extend((TC11_Register_001.Test(), TC12_Register_002.Test(), TC13_Register_003.Test(), TC14_Register_004.Test(), TC15_Register_005.Test(), TC16_Register_006.Test(), TC17_Register_007.Test(), TC18_Login_001.Test(), TC19_Login_002.Test(), TC20_Login_003.Test()))
+        self.test_cases.extend((TC11_Register_001.Test(), TC12_Register_002.Test(), TC13_Register_003.Test(), TC14_Register_004.Test(), TC15_Register_005.Test(), TC16_Register_006.Test(), TC17_Register_007.Test(), TC18_Login_001.Test(), TC19_Login_002.Test(), TC20_Login_003.Test(), TC21_Register_008.Test()))
         
     def startSuite(self):
         totalTests = len(self.test_cases)
